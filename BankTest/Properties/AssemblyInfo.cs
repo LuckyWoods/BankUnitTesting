828ca75c4074a43365c9d06c9fc2b60c399304c7
Lucky Woods <lucjwood@ut.utm.edu>
2022-04-27 21:40:23 -0500

    Add project files.

---
+++ BankTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1f559a5c-f8bb-402a-9b47-69af73015d0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BankTest/UnitTest1.cs
@@ -0,0 +1,160 @@
+﻿/*
+* Author: Lucky Jaxxon Woods
+* Date: 4/25/22
+* File: UnitTest1.cs
+* Description: A C# file containing the implementation of UnitTest1
+*
+*/
+using BankAccountNS;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        // unit test code
+
+        // ValidAmount Unit Tests
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act  
+            account.Debit(debitAmount);
+
+            // assert  
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void Credit_WithValidAmount_UpdatesBalance()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double creditAmount = 4.55;
+            double expected = 16.54;
+            BankAccount account = new BankAccount("Mr. Lucky Woods", beginningBalance);
+
+            // act  
+            account.Credit(creditAmount);
+
+            // assert  
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+
+
+        //Frozen Unit Test
+
+        [TestMethod]
+        public void Credit_Frozen()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double creditAmount = 4.55;
+            BankAccount account = new BankAccount("Mr. Lucky Woods", beginningBalance);
+
+            
+            // assert  
+            account.ToggleFreeze();
+            try
+            {
+                account.Credit(creditAmount);
+            }
+
+            catch (Exception e)
+            {
+                // assert  
+                StringAssert.Contains(e.Message, "Account frozen");
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+
+        //Less than Unit Tests.
+
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act  
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert  
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+        [TestMethod]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Credit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double creditAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Lucky Woods", beginningBalance);
+
+            // act  
+            try
+            {
+                account.Credit(creditAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert  
+                StringAssert.Contains(e.Message, BankAccount.CreditAmountLessThanZeroMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+
+
+        //More than Unit Tests.
+
+        [TestMethod]
+        //[ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Debit_WhenAmountIsMoreThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // arrange  
+            double beginningBalance = 11.99;
+            double debitAmount = 100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // act  
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                // assert  
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+            Assert.Fail("No exception was thrown.");
+        }
+
+        
+
+    }
+}
